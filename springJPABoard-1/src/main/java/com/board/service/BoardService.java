8ed312578838b46db46826a030c443873be32cc6
dldydtjr003 <dldydtjr002@gmail.com>
2026-01-29 14:40:37 +0900

    [수정,추가] 컨트롤러 수정 및 서비스 생성

---
+++ springJPABoard-1/src/main/java/com/board/service/BoardService.java
@@ -0,0 +1,16 @@
+package com.board.service;
+
+import java.util.List;
+
+import com.board.domain.Board;
+
+public interface BoardService {
+
+	//2. interface로 curd 서비스 생성
+	public int register(Board board) throws Exception; 
+	public Board read(Board board) throws Exception; 
+	public int modify(Board board) throws Exception; 
+	public int remove(Board board) throws Exception; 
+	public List<Board> list() throws Exception; 
+	public List<Board> search(String searchType, String keyword) throws Exception; 
+}
