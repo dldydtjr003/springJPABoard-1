137156c132f1660395cde4fa1e46c4e97c0848df
dldydtjr003 <dldydtjr002@gmail.com>
2026-01-29 12:22:57 +0900

    [추가] application에 환경설정 추가

---
+++ springJPABoard-1/src/main/java/com/board/SpringJpaBoard1Application.java
@@ -0,0 +1,13 @@
+package com.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaBoard1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaBoard1Application.class, args);
+	}
+
+}
